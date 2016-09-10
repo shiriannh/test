a9573e23b4cbf765c12b6f11732298800aac4c15
codeur <codeur@localhost>
2016-09-10 14:53:15 +0200

    qsf

--- test/vendor/tests/CalculatorTest.php
+++ test/vendor/tests/CalculatorTest.php
@@ -1,24 +1,20 @@
 <?php
 require 'Calculator.php';
+use PHPUnit\Framework\TestCase;
  
-class CalculatorTests extends PHPUnit_Framework_TestCase
+class CalculatorTests extends TestCase
 {
-    private $calculator;
- 
-    protected function setUp()
+ public function testPushAndPop()
     {
-        $this->calculator = new Calculator();
-    }
- 
-    protected function tearDown()
-    {
-        $this->calculator = NULL;
-    }
- 
-    public function testAdd()
-    {
-        $result = $this->calculator->add(1, 2);
-        $this->assertEquals(3, $result);
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[count($stack)-1]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
     }
  
 }
